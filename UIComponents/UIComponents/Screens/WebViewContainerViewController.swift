0d2d3d788a4e271ed980801ca144301027fe7945
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-01-12 00:23:04 +0300

    Add HTML button

--- UIComponents/UIComponents/Screens/WebViewContainerViewController.swift
+++ UIComponents/UIComponents/Screens/WebViewContainerViewController.swift
@@ -62,6 +62,7 @@ class WebViewContainerViewController: UIViewController {
 
     @IBAction func reloadButtonTapped(_ sender: UIBarButtonItem) {
         webView.reload()
+        
     }
     
     @IBAction func goBackButtonTapped(_ sender: UIBarButtonItem) {
@@ -75,6 +76,10 @@ class WebViewContainerViewController: UIViewController {
     @IBAction func openInSafariButtonTapped(_ sender: UIBarButtonItem) {
         UIApplication.shared.open(webView.url!)
     }
+    
+    @IBAction func htmlButtonTapped(_ sender: UIBarButtonItem) {
+        webView.loadHTMLString(htmlString, baseURL: nil)
+    }
 }
 
 extension WebViewContainerViewController: WKNavigationDelegate {
@@ -84,3 +89,46 @@ extension WebViewContainerViewController: WKNavigationDelegate {
 extension WebViewContainerViewController: WKUIDelegate {
 
 }
+
+private let htmlString = """
+<!doctype html>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+    <head>
+        <style>
+            body {
+                font-size: 24px;
+                font-family: "AmericanTypeWriter";
+                
+                text-align: center;
+                height: 100vh;
+                display:flex;
+            }
+            
+            .container{
+                align-items: center;
+                justify-content: center;
+                
+                width:90vw;
+                height: 90vh;
+                margin: auto;
+            }
+            
+            .element{
+                margin: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="element">
+                <h3>Hello, WKWebView!</h3><br>
+                This HTML page is using "AmericanTypeWriter" font.<br><br>
+                To go back to main page tap <a href="https://www.google.com">here.</a>
+            </div>
+        </div>
+        
+    </body>
+</html>
+"""
