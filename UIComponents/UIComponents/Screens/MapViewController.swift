2e60b2d7803ab63ad17c410aea8f36ebcb7a9ec3
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-01-14 22:47:25 +0300

    Minor updates

--- UIComponents/UIComponents/Screens/MapViewController.swift
+++ UIComponents/UIComponents/Screens/MapViewController.swift
@@ -11,11 +11,9 @@ import MapKit
 class MapViewController: UIViewController {
     
     // MARK: - IBOutlets
-    
     @IBOutlet weak var mapView: MKMapView!
     
     // MARK: - viewDidLoad
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
